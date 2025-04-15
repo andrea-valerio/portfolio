95c891006be6c3c8e884a0a6b39a3bdae5e135ea
Andrea Valerio <andrea@icio.it>
2025-04-15 22:50:10 +0200

    navigation resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import GroovePage from './pages/GroovePage'
 import OvenConfPage from './pages/OvenConfPage'
@@ -7,18 +7,32 @@ import CitInstPage from './pages/CitInstPage'
 import EcomuseoPage from './pages/EcomuseoPage'
 import SustSmokPage from './pages/SustSmokPage'
 import ReelsFypPage from './pages/ReelsFypPage'
+import { useEffect } from 'react'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/groove" element={<GroovePage />} />
-      <Route path="/ovenconf" element={<OvenConfPage />} />
-      <Route path="/citinst" element={<CitInstPage />} />
-      <Route path="/ecomuseo" element={<EcomuseoPage />} />
-      <Route path="/sustsmok" element={<SustSmokPage />} />
-      <Route path="/reelsfyp" element={<ReelsFypPage />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/groove" element={<GroovePage />} />
+        <Route path="/ovenconf" element={<OvenConfPage />} />
+        <Route path="/citinst" element={<CitInstPage />} />
+        <Route path="/ecomuseo" element={<EcomuseoPage />} />
+        <Route path="/sustsmok" element={<SustSmokPage />} />
+        <Route path="/reelsfyp" element={<ReelsFypPage />} />
+      </Routes>
+    </>
   )
 }
 
