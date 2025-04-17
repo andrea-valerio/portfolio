b5935342c258c0a5ee51430f08626ae53b0c141b
Andrea Valerio <andrea@icio.it>
2025-04-17 21:17:50 +0200

    navigation flickering solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect, useRef, useLayoutEffect } from 'react'
 import HomePage from './pages/HomePage'
 import GroovePage from './pages/GroovePage'
 import OvenConfPage from './pages/OvenConfPage'
@@ -7,32 +8,38 @@ import CitInstPage from './pages/CitInstPage'
 import EcomuseoPage from './pages/EcomuseoPage'
 import SustSmokPage from './pages/SustSmokPage'
 import ReelsFypPage from './pages/ReelsFypPage'
-import { useEffect } from 'react'
-
-function ScrollToTop() {
-  const { pathname } = useLocation()
-
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [pathname])
-
-  return null
-}
 
 function App() {
+  const location = useLocation()
+  const scrollPos = useRef<{ [path: string]: number }>({})
+
+  useEffect(() => {
+    const handleScroll = () => {
+      scrollPos.current[location.pathname] = window.scrollY
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [location.pathname])
+
+  useLayoutEffect(() => {
+    if (location.pathname === '/') {
+      const y = scrollPos.current['/'] || 0
+      window.scrollTo(0, y)
+    } else {
+      window.scrollTo(0, 0)
+    }
+  }, [location.pathname])
+
   return (
-    <>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/groove" element={<GroovePage />} />
-        <Route path="/ovenconf" element={<OvenConfPage />} />
-        <Route path="/citinst" element={<CitInstPage />} />
-        <Route path="/ecomuseo" element={<EcomuseoPage />} />
-        <Route path="/sustsmok" element={<SustSmokPage />} />
-        <Route path="/reelsfyp" element={<ReelsFypPage />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/groove" element={<GroovePage />} />
+      <Route path="/ovenconf" element={<OvenConfPage />} />
+      <Route path="/citinst" element={<CitInstPage />} />
+      <Route path="/ecomuseo" element={<EcomuseoPage />} />
+      <Route path="/sustsmok" element={<SustSmokPage />} />
+      <Route path="/reelsfyp" element={<ReelsFypPage />} />
+    </Routes>
   )
 }
 
