0ba798f32dedc820c5a8a55c5d90486679c6b52d
Andrea Valerio <andrea@icio.it>
2025-04-18 17:39:42 +0200

    publication commit 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/portfolio/',
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/portfolio/',
   plugins: [react()],
-})
+}))
