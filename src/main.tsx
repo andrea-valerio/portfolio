fb7880e5e96900a5c2eb6d4d9ae71c466ba601c8
Andrea Valerio <andrea@icio.it>
2025-04-18 19:15:16 +0200

    publication commit 7

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 
-const isDev = import.meta.env.DEV
-const basename = isDev ? '/' : '/portfolio'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter  basename={basename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
